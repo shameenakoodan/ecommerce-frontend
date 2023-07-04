eedfebc96d6ff03eefa0611f389e38e91ada882b
Shameena Koodan <shameena.koodan@gmail.com>
2023-07-04 00:32:51 -0500

    Created a footer and styled it

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const url = 'https://real-time-amazon-data.p.rapidapi.com/search?query=Phone&page=1&country=US&category_id=aps';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bb72b8974amsh185e8995e5f31dbp1f741ajsnff6647acb9c3',
+		'X-RapidAPI-Host': 'real-time-amazon-data.p.rapidapi.com'
+	}
+};
+try {
+	const response = fetch(url, options).then((res)=>{
+        return res.json();
+    }).then((data)=>{
+        console.log(data);
+    })
+} catch (error) {
+	console.error(error);
+}
