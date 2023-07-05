a484097db83b1ca85c5334122d3d94ed315293f0
Shameena Koodan <shameena.koodan@gmail.com>
2023-07-05 09:55:34 -0500

    Added custom message for succesfull submission of contact form

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,21 +1,4 @@
 function submissionmMessage() {
-
-   /*
-
-   Get Response from SERVER using AJAX.
-
-   */
-
-  /* document.getElementById("success").style.display = "block";
-   const div = document.createElement("div");
-   div.innerHTML="Success";
-  // console.log(body);
    document.getElementById("contactform").style.display="none";
-   const form = document.getElementById("form");
-   document.form.appendChild(div);*/
-   var elemDiv = document.createElement("div");
-   elemDiv.innerHTML="Submitted succesfully";
-elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;font-soze:20px';
- document.getElementById("form").appendChild(elemDiv);
- document.getElementById("contactform").style.display="none";
+   document.getElementById("success").style.display="block";
 }
