a74fcd793f5232fdf5100fbedceaee70eb4c37c0
Shameena Koodan <shameena.koodan@gmail.com>
2023-07-05 01:27:36 -0500

    Added Js for contact form

--- src/js/dist/contact.dev.js
+++ src/js/dist/contact.dev.js
@@ -1,9 +1,8 @@
+"use strict";
+
 function submissionmMessage() {
-
-   /*
-
+  /*
    Get Response from SERVER using AJAX.
-
    */
 
   /* document.getElementById("success").style.display = "block";
@@ -13,9 +12,9 @@ function submissionmMessage() {
    document.getElementById("contactform").style.display="none";
    const form = document.getElementById("form");
    document.form.appendChild(div);*/
-   var elemDiv = document.createElement("div");
-   elemDiv.innerHTML="Submitted succesfully";
-elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;font-soze:20px';
- document.getElementById("form").appendChild(elemDiv);
- document.getElementById("contactform").style.display="none";
+  var elemDiv = document.createElement("div");
+  elemDiv.innerHTML = "Submitted succesfully";
+  elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;font-soze:20px';
+  document.getElementById("form").appendChild(elemDiv);
+  document.getElementById("contactform").style.display = "none";
 }
