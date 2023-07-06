6fa0844ed8cfec3ca8166a5f1cfc83e11cfe973c
Shameena Koodan <shameena.koodan@gmail.com>
2023-07-06 00:24:48 -0500

    Added hyperlinks for signup page

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,3 +2,6 @@ function submissionmMessage() {
    document.getElementById("contactform").style.display="none";
    document.getElementById("success").style.display="block";
 }
+function changehyperlink(){
+   document.getElementById("link1").href = ".."
+}
---
+++ src/js/dist/contact.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function submissionmMessage() {
+  document.getElementById("contactform").style.display = "none";
+  document.getElementById("success").style.display = "block";
+}
+
+function changehyperlink() {
+  document.getElementById("link1").href = "..";
+}
